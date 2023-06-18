f3655d3559da98f246e55e9de1256e585779f6d8
Matheus_Silas <matheus.silas123@gmail.com>
2023-06-17 22:41:51 -0300

    feat(letsdrink) configuração para segurança

--- app/src/main/java/com/example/letsdrink/data/remote/client/ApiFactory.kt
+++ app/src/main/java/com/example/letsdrink/data/remote/client/ApiFactory.kt
@@ -1,7 +1,11 @@
 package com.example.letsdrink.data.remote.client
 
+import com.example.letsdrink.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.Interceptor.Chain
 import okhttp3.OkHttpClient
 import okhttp3.OkHttpClient.Builder
+import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -9,19 +13,21 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object Apifactory {
 
-    private const val BASE_URL = "https://rails-production-72ec.up.railway.app/"
-
-
     private fun retrofit(): Retrofit {
         val logging = HttpLoggingInterceptor()
         logging.setLevel(HttpLoggingInterceptor.Level.BODY)
         val client: OkHttpClient = Builder()
             .addInterceptor(logging)
+            .addInterceptor(Interceptor { chain ->
+                val request = chain.request().newBuilder()
+                    .addHeader("Authorization", BuildConfig.PASSWORD).build()
+                chain.proceed(request = request)
+            })
             .build()
 
 
         return Retrofit.Builder()
-            .baseUrl(BASE_URL)
+            .baseUrl(BuildConfig.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .client(client)
             .build()
